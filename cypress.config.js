c5c68dcb907430e83a1728607782576cecfb8ea9
nthmi1296 <nthmi1296@gmail.com>
2023-05-25 12:14:15 +0700

    Installation, Configuration. DOM Terminology. Tests Structure. Types of Locators. Finding Web Elements.

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    // baseUrl: 'http://example.cypress.io',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
+  },
+})
---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress" />
+
+describe('Our first suite', () => {
+    it('first test', () =>{
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        
+        // by Tag Name
+        cy.get('input')
+
+        // by ID
+        cy.get('#inputEmail1')
+
+        // by Class name
+        cy.get('.input-full-width')
+
+        // by Attribute name
+        cy.get('[placeholder]')
+
+        // by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        // by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by Tag name and Attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        // by two different attributes
+        cy.get('[placeholder="Email"][type="email"]')
+
+        // by Tag name, Attribute with value, ID and Class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        // The most recommended way by Cypress
+        cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it.only('second test', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card','Horizontal form').find('[type="email"]')
+
+    })
+})
+
